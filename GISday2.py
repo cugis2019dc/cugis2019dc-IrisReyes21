15bfc4f54a6f302ab73185128d8e10e337c66683
IrisReyes21 <53440559+IrisReyes21@users.noreply.github.com>
2019-07-31 10:08:59 -0400

    Add files via upload

---
+++ GISday2.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 30 10:50:30 2019
+
+@author: STEM
+"""
+
+import math
+
+dir(math)
+
+math.pi
+
+math.factorial(0)
+
+import math 
+
+dir(math)
+
+math.sin(math.pi/180)
+
+def CubeRoot(a):
+    CubeRoot = a**1/3
+    print(CubeRoot)
+    
+CubeRoot(27)
+    
+    
+CubeRoot(8)
+    
+n=int(3)
+d=int(9)
+x=1/n
+print ("your answer=" ,d**x)
+
+import math
+
+dir(math)    
+
+
+
+a = int(input("Please enter a value for your varible"))
+
+def CubeRoot(a):
+    CubeRoot = (a**(1/3))
+    print(CubeRoot)
+    
+CubeRoot(a)
+
+print("Your answer is", CubeRoot)
+
+
+a = int(input("enter a value"))
+
+def Toast(a):
+    Toast = math.pow(a,1/3)
+    print("The cubic root of" ,a, "is" ,Toast)
+    
+Toast(a)
